bc7d983efeb79e8adb618594bca5fa34aa81f4b9
Sarah Angyu <angyusarah1@gmail.com>
2023-08-25 11:48:33 +0100

    imageCarousel

---
+++ imageCarousel.js
@@ -0,0 +1,5 @@
+let lastParagraph = document.getElementById("last");
+//console.log(lastParagraph);
+
+lastParagraph.innerHTML =
+  "Bright orange with three distinctive white bars, clown anemonefish are among the most recognizeable of all reefdwellers. They reach about 11 centimeters in length, and are named for the multicolored sea anaemone in which they made their homes.";
